78fc955f675656f7e568a7a0a1ae2adfb2e7588c
sadikuernest00-prog <sadikuernest00@gmail.com>
2025-10-23 03:44:58 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
+  output: 'server', // ← this makes Astro run with dynamic routes
 });
