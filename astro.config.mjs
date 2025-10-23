01384bdaadcacae4a49c0ddcc6f1c72507186616
sadikuernest00-prog <sadikuernest00@gmail.com>
2025-10-23 04:46:07 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // important for SSR
-  adapter: netlify(),
+  output: 'static',
+  integrations: [tailwind()],
 });
