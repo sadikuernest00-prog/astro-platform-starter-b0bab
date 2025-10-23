01b5bb539f49302b9a27b0542e9d76aa2384abf7
sadikuernest00-prog <sadikuernest00@gmail.com>
2025-10-23 04:13:30 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/static';
 
-// ✅ Static build (no server functions)
 export default defineConfig({
-  output: 'static',
-  adapter: netlify(),
   site: 'https://amicbridge.com',
+  output: 'static',
 });
