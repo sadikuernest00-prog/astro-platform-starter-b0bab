e8ef95a49305a3289a6e3d71b98375a9583b7138
sadikuernest00-prog <sadikuernest00@gmail.com>
2025-10-23 04:25:31 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://amicbridge.com',
-  output: 'static',
+  output: 'server', // important for SSR
+  adapter: netlify(),
 });
